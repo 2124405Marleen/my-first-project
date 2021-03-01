7a2fa1c4c6453579d0b6bec3a649451c128af8a6
Marleen <mroubos2@avans.nl>
2021-03-01 09:24:24 +0100

    ToH6: add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,8 +21,8 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService) { }
 
-  /** GET heroes from the server */
- /** GET heroes from the server */
+ //  /** GET heroes from the server */
+ // /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
@@ -31,7 +31,7 @@ export class HeroService {
       );
   }
 
-  /** GET hero by id. Will 404 if id not found */
+  // /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
@@ -39,18 +39,18 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
-  
-  /** Log a HeroService message with the MessageService */
+
+  // /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
 
-  /**
-  * Handle Http operation that failed.
-  * Let the app continue.
-  * @param operation - name of the operation that failed
-  * @param result - optional value to return as the observable result
-  */
+  // /**
+  // * Handle Http operation that failed.
+  // * Let the app continue.
+  // * @param operation - name of the operation that failed
+  // * @param result - optional value to return as the observable result
+  // */
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
@@ -65,11 +65,19 @@ export class HeroService {
     };
   }
 
-  /** PUT: update the hero on the server */
+  // /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  // /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
     );
   }
 }
